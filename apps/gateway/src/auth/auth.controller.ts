6475afba52a6b0371067238e8ee185a096fc5f31
Theekshana Wijerathne <theekshanawijerathne101@gmail.com>
2025-10-11 15:10:17 +0530

    Update cookie domain to allow subdomains for access token

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -175,7 +175,7 @@ export class AuthController {
     secure: false,         
     sameSite: 'none' as const, 
     path: '/',
-    domain: 'localhost',
+    domain: '.localhost',
   };
 
   res.cookie('access_token', access_token, cookieOptions);
