27ecc8f63f8f9269b0f4de0aaca75e3bb41fb971
Theekshana Wijerathne <theekshanawijerathne101@gmail.com>
2025-10-13 00:43:55 +0530

    Fix CORS origin URL by removing trailing slash

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieparser())
   
   app.enableCors({
-    origin: ['https://learniedu.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://learniedu.vercel.app', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
