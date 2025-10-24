9f748e9bc28385cf6af0a24cdd5b6190fc84dda6
Erandathee Wijeratne <erandatheewijeratne@gmail.com>
2025-10-25 01:58:37 +0530

    Final improvements erandathee (#81)
    * Refactor daily active users logic to use Date object directly for DB lookup and remove unnecessary dayLabel variable
    
    * Refactor deleteGroup method to use req object for user ID retrieval
    
    * Refactor daily active users and engagement logging; update cron job schedules and remove unused date field

--- apps/analytics-ms/src/analytics-ms.service.ts
+++ apps/analytics-ms/src/analytics-ms.service.ts
@@ -232,11 +232,12 @@ export class AnalyticsMsService {
       dailyRecord.active_users = activeUserCount;
     } else {
       dailyRecord = this.dailyActiveUsersRepo.create({
-        date: start,
         active_users: activeUserCount,
         total_users: 0,
         engagement: 0,
       });
+
+      await this.dailyActiveUsersRepo.save(dailyRecord);
     }
 
     await this.dailyActiveUsersRepo.save(dailyRecord);
@@ -272,7 +273,6 @@ export class AnalyticsMsService {
       dailyRecord.active_users = activeUserCount;
     } else {
       dailyRecord = this.dailyActiveUsersRepo.create({
-        date: start,
         active_users: activeUserCount,
       });
     }
@@ -323,13 +323,13 @@ export class AnalyticsMsService {
 
   // -- Crohn Job to calculate daily active users + engagement for the past day
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron('22 23 * * *')
   async recordDailyActiveUsers() {
     this.logger.log('Running daily active user aggregation job...');
     await this.calculateAndStoreDailyActiveUsers(); // no params → uses yesterday by default
   }
 
-  @Cron('30 0 * * *') // Runs at 12:30 AM every day
+  @Cron('23 23 * * *') // Runs at 12:30 AM every day
   async recordDailyEngagement() {
     this.logger.log('Running daily engagement aggregation job...');
     await this.calculateAndStoreDailyEngagement(); // no params → uses yesterday by default
@@ -1092,6 +1092,7 @@ export class AnalyticsMsService {
           description: activity.description,
           time: this.getTimeDifference(activity.created_at),
           created_at: activity.created_at,
+          metadata: activity.metadata,
         };
       });
 
